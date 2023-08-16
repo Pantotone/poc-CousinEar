ea52b6969734481fdad0a967f6151c1ff5994c74
Panto! <guustavopantoja@gmail.com>
2023-08-15 21:17:13 -0300

    feat: basic discord.js login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hiiii");
+import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
