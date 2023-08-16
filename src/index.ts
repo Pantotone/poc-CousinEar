aae58fcc95477659712aee2a0a63543bbd9bcd72
Panto! <guustavopantoja@gmail.com>
2023-08-16 00:46:53 -0300

    lint: change from tab to spaces

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ import { joinVoiceChannel } from "@discordjs/voice";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, async c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(`Ready! Logged in as ${c.user.tag}`);
 
-	const voiceChannel = process.env.DISCORD_TEST_VOICECHANNEL_ID ? await client.channels.fetch(process.env.DISCORD_TEST_VOICECHANNEL_ID) as VoiceChannel : undefined;
-	const guild = process.env.DISCORD_TEST_GUILD_ID ? await client.guilds.fetch(process.env.DISCORD_TEST_GUILD_ID) : undefined;
+    const voiceChannel = process.env.DISCORD_TEST_VOICECHANNEL_ID ? await client.channels.fetch(process.env.DISCORD_TEST_VOICECHANNEL_ID) as VoiceChannel : undefined;
+    const guild = process.env.DISCORD_TEST_GUILD_ID ? await client.guilds.fetch(process.env.DISCORD_TEST_GUILD_ID) : undefined;
 
-	if(guild && voiceChannel) {
-		joinVoiceChannel({
-			channelId: voiceChannel.id,
-			guildId: guild.id,
-			adapterCreator: guild.voiceAdapterCreator,
-			selfDeaf: false,
-			selfMute: true,
-		});
-	}
+    if(guild && voiceChannel) {
+        joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: guild.id,
+            adapterCreator: guild.voiceAdapterCreator,
+            selfDeaf: false,
+            selfMute: true,
+        });
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
