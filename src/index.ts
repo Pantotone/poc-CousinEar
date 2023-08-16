c83ee19966b5bac193c3490662e8f6dd34e8b308
Panto! <guustavopantoja@gmail.com>
2023-08-16 04:13:50 -0300

    feat: create a voice recorder to pcm

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import "dotenv/config";
-import { Client, Events, GatewayIntentBits, VoiceChannel } from "discord.js";
-import { joinVoiceChannel } from "@discordjs/voice";
+import { Client, Events, GatewayIntentBits, GuildMember, VoiceChannel } from "discord.js";
+import { EndBehaviorType, getVoiceConnection, joinVoiceChannel } from "@discordjs/voice";
+import path from "node:path";
+import { pipeline } from "node:stream";
+import os from "node:os";
+import fs from "node:fs";
+import * as prism from "prism-media";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const temporaryFolder = path.join(os.tmpdir(), "cousinear");
+const recordingFolder = path.join(temporaryFolder, "recordings");
+
+fs.mkdir(recordingFolder, { recursive: true }, () => {
+    console.log(`Created temporary recording folder - ${recordingFolder}`);
+});
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
 client.once(Events.ClientReady, async c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
@@ -11,14 +23,55 @@ client.once(Events.ClientReady, async c => {
     const guild = process.env.DISCORD_TEST_GUILD_ID ? await client.guilds.fetch(process.env.DISCORD_TEST_GUILD_ID) : undefined;
 
     if(guild && voiceChannel) {
-        joinVoiceChannel({
+        const voiceConnection = joinVoiceChannel({
             channelId: voiceChannel.id,
             guildId: guild.id,
             adapterCreator: guild.voiceAdapterCreator,
             selfDeaf: false,
             selfMute: true,
         });
+        
+        voiceConnection.receiver.speaking.on("start", userId => {
+            const member = voiceChannel.members.get(userId);
+            if(!member) return;
+            
+            record(guild.id, member);
+        });
     }
 });
+
+
+function record(guildId: string, member: GuildMember) {
+    const voiceConnection = getVoiceConnection(guildId);
+    if(!voiceConnection) throw new Error(`There's no voice connection with guild ${guildId}`);
+
+    const startTime = Date.now();
+    const recordingFilePath = path.join(recordingFolder, `${member.id}-${startTime}.pcm`);
+
+    const opusStream = voiceConnection.receiver.subscribe(member.id, {
+        end: {
+            behavior: EndBehaviorType.AfterSilence,
+            duration: 1000
+        }
+    });
+
+    const pcmStream = new prism.opus.Decoder({
+        frameSize: 960, channels: 2, rate: 48000
+    });
+
+    // ffmpeg -f s16le -ar 48000 -ac 2 -i merge.pcm out.mp3
+
+    const out = fs.createWriteStream(recordingFilePath);
+
+    console.log(`Started recording: ${member.user.displayName} - ${startTime}`);
+
+    pipeline(opusStream, pcmStream, out, (err) => {
+        if (err) {
+            console.warn(`Error recording file ${recordingFilePath} - ${err.message}`);
+        } else {
+            console.log(`Recorded ${recordingFilePath}`);
+        }
+    });
+}
 
 client.login(process.env.DISCORD_TOKEN);
