584df161d66e229979503026d7c3e0cfce5eeee7
Panto! <guustavopantoja@gmail.com>
2023-08-17 04:09:05 -0300

    feat: make whisper call based on env settings

--- src/methods/CallWhisperCLI.ts
+++ src/methods/CallWhisperCLI.ts
@@ -4,9 +4,20 @@ import { exec } from "child_process";
 import path from "node:path";
 
 export default async function CallWhisperCLI(audioFilePath: string): Promise<string> {
+
+    const language = process.env.WHISPER_LANGUAGE || "Portuguese";
+    const computeType = process.env.WHISPER_COMPUTE_TYPE || "int8";
+    const model = process.env.WHISPER_MODEL || "small";
+
+    const cli = process.env.WHISPER_CLI || "whisper";
+    const cliCommands: {[key: string]: string} = {
+        "whisper": `whisper --model ${model} --language ${language} --fp16 ${computeType.toLowerCase() === "float16" ? "True" : "False"} ${audioFilePath} --output_dir ${transcriptionsFolder} --output_format txt`,
+        "whisper-ctranslate2": `whisper-ctranslate2 --model ${model} --language ${language} --compute_type ${computeType} ${audioFilePath} --output_dir ${transcriptionsFolder} --output_format txt`,
+    };
+
     return new Promise((resolve, reject) => {
 
-        exec(`whisper-ctranslate2 --model small --language Portuguese --compute_type int8 ${audioFilePath} --output_dir ${transcriptionsFolder} --output_format txt`, async (err, stdout, stderr) => {
+        exec(cliCommands[cli] || cliCommands["whisper"], async (err, stdout, stderr) => {
             if(err || stderr) {
                 reject();
             }
