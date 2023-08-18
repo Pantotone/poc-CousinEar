d2765183721072945f7cda936ee49fa200b476bc
Panto! <guustavopantoja@gmail.com>
2023-08-18 06:38:26 -0300

    feat: block bots from triggering a transcription

--- src/handlers/OnMemberSpeaking.ts
+++ src/handlers/OnMemberSpeaking.ts
@@ -3,6 +3,8 @@ import crypto from "node:crypto";
 import processSpeak from "../services/speech-to-text";
 
 export async function onMemberSpeaking(voiceChannel: VoiceChannel, member: GuildMember) {
+    if(member.user.bot) return;
+    
     const id = crypto.randomUUID();
     processSpeak(id, voiceChannel, member);
 }
